26e5c459f379d09665610d857060b4ec88301189
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-04-19 22:09:26 -0300

    se commitean cambios de clase 1, clase 2 y tp0

---
+++ 00-pruebas/02-clase 2/bb.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+    return 0;
+}
+
+int bb(int[] arreglo, int inicio, int fin){
+    medio = len(arreglo) / 2
+     
+    int[] izq = arreglo[inicio, medio-1]
+    
+    int[] der = arreglo[medio, fin]
+    return x;
+}
