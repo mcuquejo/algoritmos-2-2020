1ac33cc15e6705431d07569fd4a461bef507ef8e
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-05-17 19:21:13 -0300

    Se ajusta dyc_00 por problema en llamada recursiva

--- 00-pruebas/dyc_00.c
+++ 00-pruebas/dyc_00.c
@@ -1,5 +1,6 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include "dyc_00.h"
 // Implementar un algoritmo en C que reciba un arreglo de enteros de tamaño n, 
 // ordenado ascendentemente y sin elementos repetidos, y determine en O(log⁡ n)
 // si es mágico. 
@@ -13,34 +14,28 @@
 
 //     B = [ 1, 2, 4, 6, 7, 9 ] no es mágico porque B[i] != i para todo i.
 
-bool es_magico(int arreglo[], int tam, int inicio, int fin) {
-    if (inicio > fin) return false;
+bool es_magico(int arreglo[], int inicio, int fin) {    
+    if (inicio > fin) return false;    
 
     int medio = (inicio + fin) / 2;
 
     if (arreglo[medio] == medio) {
         return true;
+    }    
+    if (arreglo[medio] > medio) {
+        return es_magico(arreglo, inicio, medio - 1);
     }
-    bool mitad_iz = es_magico(arreglo, medio, inicio, medio);
-    bool mitad_der = es_magico(arreglo, fin - medio, medio, fin);
+    if (arreglo[medio] < medio) {
+        return es_magico(arreglo, medio + 1, fin);
+    }
+    
     return false;
 }
 
-int main(void) {
-    int arreglo1[] = {-3, 0, 1, 3, 7, 9};
-    int tam1 = 6;
-    int arreglo2[] = {1, 2, 4, 6, 7, 9};
-    int tam2 = 6;
-    if (es_magico(arreglo1, tam1, 0, tam1)) {
-        printf("Arreglo 1 Es magico\n");
+void probar_es_magico(int* arreglo, int tam) {
+    if (es_magico(arreglo, 0, tam)) {
+        printf("Arreglo Es magico\n");
     } else {
-        printf ("Arreglo 1 No es magico\n");
+        printf ("Arreglo No es magico\n");
     }
-
-    if (es_magico(arreglo2, tam2, 0, tam2)) {
-        printf("Arreglo 2 Es magico\n");
-    } else {
-        printf ("Arreglo 2 No es magico\n");
-    }
-    return 0;
 }
