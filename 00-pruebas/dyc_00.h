388c2ee2c909a181b9046204cd13879eb136614b
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-05-17 19:21:29 -0300

    Se crea dyc_00.h

---
+++ 00-pruebas/dyc_00.h
@@ -0,0 +1,11 @@
+// Implementar un algoritmo en C que reciba un arreglo de enteros de tamaño n, 
+// ordenado ascendentemente y sin elementos repetidos, y determine en O(log⁡ n)
+// si es mágico. 
+//Un arreglo es mágico si existe algún valor i tal que 0 ≤ i < n y arr[i] = i. 
+
+//Justificar el orden del algoritmo.
+
+// Ejemplos:
+
+//     A = [ -3, 0, 1, 3, 7, 9 ] es mágico porque A[3] = 3.
+void probar_es_magico(int* arreglo, int tam);
