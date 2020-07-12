17a7345ad01e81e61ba1ebcb51e38af491ab8f99
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-07-12 17:10:52 -0300

    se agrega calculadora_poalca a main

--- 08-tp1/main.c
+++ 08-tp1/main.c
@@ -1,4 +1,5 @@
 #include "strutil.h"
+#include "dc.h"
 #include "testing.h"
 #include <stdio.h>
 
@@ -13,7 +14,8 @@ void pruebas_cola_catedra();
 int main(void) {
     /* Ejecuta todas las pruebas unitarias. */
     printf("~~~ PRUEBAS ALUMNO ~~~\n");
-    pruebas_strutil_alumno();    
+    // pruebas_strutil_alumno();
+    calculadora_polaca();    
 
     return failure_count() > 0;
 }
