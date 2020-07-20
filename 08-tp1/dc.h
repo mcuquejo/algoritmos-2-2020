1b3eec9c16875929b6bc0df4990fab81d2522cf6
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-07-19 21:41:23 -0300

    Se agregan al header primitivas que seran utilizadas por infix

--- 08-tp1/dc.h
+++ 08-tp1/dc.h
@@ -1,1 +1,7 @@
+#include <stdbool.h>
+#include "pila.h"
+
+bool es_numero(char* cadena);
+void free_pila(pila_t* pila_operaciones);
+char* formatear_operacion(char* operacion);
 int calculadora_polaca();
