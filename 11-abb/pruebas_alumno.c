67fa7cd97d00c1964c1bffc694ceaa2f235272cb
Mauro Cuquejo <mcuquejo@fi.uba.ar>
2020-08-01 10:30:53 -0300

    Se crean primeras pruebas alumno ABB

---
+++ 11-abb/pruebas_alumno.c
@@ -0,0 +1,39 @@
+#include "abb.h"
+#include "testing.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>  // For ssize_t in Linux.
+
+
+/* ******************************************************************
+ *                        PRUEBAS UNITARIAS
+ * *****************************************************************/
+int comparar_clave (const char* clave_a, const char* clave_b) {
+    return strcmp(clave_a, clave_b);
+}
+
+static void prueba_crear_abb_vacio()
+{
+    abb_t* abb = abb_crear(comparar_clave, NULL);
+
+    print_test("Prueba abb crear abb vacio", abb);
+    print_test("Prueba abb la cantidad de elementos es 0", abb_cantidad(abb) == 0);
+    print_test("Prueba abb obtener clave A, es NULL, no existe", !abb_obtener(abb, "A"));
+    print_test("Prueba abb pertenece clave A, es false, no existe", !abb_pertenece(abb, "A"));
+    print_test("Prueba abb borrar clave A, es NULL, no existe", !abb_borrar(abb, "A"));
+
+    abb_destruir(abb);
+}
+
+/* ******************************************************************
+ *                        FUNCIÓN PRINCIPAL
+ * *****************************************************************/
+
+
+void pruebas_abb_alumno()
+{
+    /* Ejecuta todas las pruebas unitarias. */
+    prueba_crear_abb_vacio();
+}
